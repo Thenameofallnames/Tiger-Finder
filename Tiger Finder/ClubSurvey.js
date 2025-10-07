5e65030532a836c700a490dcb0cc33126e67e986
iwoq <bobowling1@icloud.com>
2025-10-07 19:48:40 -0400

    formating and comments

--- Tiger Finder/ClubSurvey.js
+++ Tiger Finder/ClubSurvey.js
@@ -1,11 +1,11 @@
-function checkClickedQ1(){
+function checkClickedQ1() {
     const activity = document.getElementById("activity");
     const academic = document.getElementById("academic");
     const volunteer = document.getElementById("volunteer");
-    
+
     const subQuestions = document.getElementsByClassName("subQuestion1");
 
-    if(activity.checked){
+    if (activity.checked) {
         // Loop through the collection of elements and set display to "block" for each
         for (let i = 0; i < subQuestions.length; i++) {
             subQuestions[i].style.display = "block";
