aa0cbf07989639f003b72b4ede0047a55685aeae
Thenameofallnames <Eze33@icloud.com>
2025-11-24 14:02:07 -0500

    init 2 (electric boogaloo)

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,20 @@
+const { watch } = require('fs');
 const path = require('path');
+const { Template } = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
     mode: 'development',
     devtool: 'eval-source-map',
-    entry: '/src/index.js',
+    entry: '/src/pack.js',
     output: {
         path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
+    },
+    watch: true,
+    plugins: [
+        new HtmlWebpackPlugin({
+            Template: './src/index.html'
+        })
+    ],
     }
-
-}
